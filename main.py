ba4af20c0aff407827441d62d5bbf8a49e75b104
Dzobamain <knopkayt26@gmail.com>
2025-04-08 18:08:48 +0200

    Added enter()

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 from replacer import replacer_in_text, replacer_in_file, replacer_in_directory
-        
+      
 def check_user_choice(user_choice):
+    array = enter(user_choice)
+    if array is None:
+        return
+
     match user_choice:
-            case '1':
-                text = input("Enter your text: ")
-                old_word = input("Enter the word to replace: ")
-                new_word = input("Enter the new word: ")
-                if text == 'q' or old_word == new_word:
-                    print("Nothing has changed")
-                    return None
-                replacer_in_text(text, old_word, new_word)
-            case '2':
-                path = input("Enter the file path: ")
-                old_word = input("Enter the word to replace: ")
-                new_word = input("Enter the new word: ")
-                if path == 'q' or old_word == new_word:
-                    print("Nothing has changed")
-                    return
-                replacer_in_file(path, old_word, new_word)
-            case '3':
-                path = input("Enter the directory path: ")
-                old_word = input("Enter the word to replace: ")
-                new_word = input("Enter the new word: ")
-                if path == 'q' or old_word == new_word:
-                    print("Nothing has changed")
-                    return
-                replacer_in_directory(path, old_word, new_word)
+        case '1':
+            replacer_in_text(array[0], array[1], array[2])
+        case '2':
+            replacer_in_file(array[0], array[1], array[2])
+        case '3':
+            replacer_in_directory(array[0], array[1], array[2])
+        case _:
+            print("Unknown choice.")
+
+                
+def enter(number):
+    if number == 'q':
+        return None
+    
+    if number == '1':
+        text_or_path = input("Enter your text: ")
+    elif number == '2' or number == '3':
+        text_or_path = input("Enter the file path: ")
+    else:
+        print("Invalid choice.")
+        return None
+
+    
+    old_word = input("Enter the word to replace: ")
+    new_word = input("Enter the new word: ")
+    
+    if  old_word == new_word:
+        return None
+    
+    return text_or_path, old_word, new_word
     
 def main():
     user_choice = ''
@@ -34,6 +44,7 @@ def main():
         print("2. File")
         print("3. Directory")
         print("q. exit")
+        
         user_choice = input("Choose!: ")
         check_user_choice(user_choice)
 
